dffb01eed51a02570ffce038add2604050ad06ea
kevin <kipronok234@gmail.com>
2024-10-21 10:45:42 +0300

    Your commit message

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <ul>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
-            {/* <li><Link to="/">Home</Link></li> */}
+            
           </ul>
         </nav>
         <Routes>
