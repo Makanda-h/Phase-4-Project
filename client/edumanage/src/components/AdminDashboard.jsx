41cf5ffb102149c5f51186aa5d9d15b0be7f5119
Hope <hopemwaura39@gmail.com>
2024-10-27 20:07:17 +0300

    stats update

--- client/edumanage/src/components/AdminDashboard.jsx
+++ client/edumanage/src/components/AdminDashboard.jsx
@@ -28,23 +28,26 @@ function AdminDashboard() {
   const [courseName, setCourseName] = useState("");
 
   useEffect(() => {
-    fetchStats();
+    updateStats();
+  }, [students, teachers, courses]);
+
+  useEffect(() => {
     fetchStudents();
     fetchTeachers();
     fetchCourses();
   }, []);
 
+  const updateStats = () => {
+    setStats({
+      totalStudents: students.length,
+      totalTeachers: teachers.length,
+      totalCourses: courses.length,
+    });
+  };
+
   const API_URL = "https://edumanage-backend.onrender.com";
 
-  const fetchStats = async () => {
-    try {
-      const response = await fetch("/api/admin/stats");
-      const data = await response.json();
-      setStats(data);
-    } catch (error) {
-      console.error("Error fetching admin stats:", error);
-    }
-  };
+  
 
   const fetchStudents = async () => {
     try {
